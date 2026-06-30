fcd7aa90ac522725076974f84b11c7bea823a979
Solangel <solangel@gmail.com>
2026-06-29 22:12:21 -0500

    Modulo2 Actualizacion2

--- src/main/java/com/example/modulo1_practica/entity/Tarea.java
+++ src/main/java/com/example/modulo1_practica/entity/Tarea.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -38,5 +40,18 @@ public class Tarea {
         this.updateAt= LocalDate.now();
     }
 
+    @ManyToOne
+    @JoinColumn(
+        name = "usuario_id",
+        referencedColumnName = "idUsuario"
+    )
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(
+        name = "categoria_id",
+        referencedColumnName = "idCategoria"
+    )
+    private Categoria categoria;
 
 }
--- src/main/java/com/example/modulo1_practica/entity/dto/TareaMapper.java
+++ src/main/java/com/example/modulo1_practica/entity/dto/TareaMapper.java
@@ -1,7 +1,7 @@
 package com.example.modulo1_practica.entity.dto;
 
 import org.mapstruct.Mapper;
-
+import org.mapstruct.Mapping;
 
 import com.example.modulo1_practica.entity.Tarea;
 
@@ -9,6 +9,9 @@ import com.example.modulo1_practica.entity.Tarea;
 public interface TareaMapper {
 
     TareaDto tareaToTareaDto(Tarea tarea);
+
+    @Mapping(target = "createAt", ignore = true)
+    @Mapping(target = "updateAt", ignore = true)
     Tarea tareaDtoToTarea(TareaDto dto);
 
 }
